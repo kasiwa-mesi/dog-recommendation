902f98e94e029227d1325fc7f76b2a6dc80ab2e7
kasiwa-mesi <italynataly@outlook.jp>
2022-07-15 10:28:43 +0900

    stop installing in tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom";
 import Home from "./pages/Home";
+import "./input.css";
 
 createRoot(document.querySelector("#content")).render(<Home />);
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
