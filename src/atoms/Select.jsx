0d34862ff8167ad336f3f8dc17931fb412af12a4
kasiwa-mesi <italynataly@outlook.jp>
2022-07-18 22:19:58 +0900

    :lipstick: make button styled

--- src/atoms/Select.jsx
+++ src/atoms/Select.jsx
@@ -1,6 +1,12 @@
 function Select(props) {
   return (
-    <select name="breed" defaultValue={props.default}>
+    <select
+      name="breed"
+      defaultValue={props.default}
+      style={{
+        fontSize: "2rem",
+      }}
+    >
       {props.list.map((breed) => {
         return(
           <option key={breed} value={breed}>
